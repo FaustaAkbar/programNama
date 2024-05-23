ec1ed023d8be564e6ef46366ce18e30e718d02d8
FaustaWSL <faussss1357@gmail.com>
2024-05-23 21:55:07 +0700

    Update

--- nama.cpp
+++ nama.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    for (int i = 0; i < 10000000000000; i++)
+    {
+        cout << "coba linux kuyyy" << endl;
+        cout << "coba linux kuyyy333" << endl;
+    }
+
     cout << "coba linux kuyyy" << endl;
     cout << "coba linux kuyyy333" << endl;
 }
